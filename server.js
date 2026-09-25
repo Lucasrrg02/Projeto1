e7c0e4e7daac61b9c491116186d19afe190aa85e
Lucasrrg <92329020+Lucasrrg02@users.noreply.github.com>
2026-09-25 10:03:21 -0300

    Os10mbai (#27)
    * Finaliza rotas do backend com tratamento do banco e admin
    
    * bug
    
    * adicionadashboardelogin
    
    * arruma10mb

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true}));
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
